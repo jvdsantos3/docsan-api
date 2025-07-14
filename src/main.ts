a48a59859cddf5f37a4fc54ab134578e91363abb
João <joao.alcantara@fabricainfo.com>
2025-07-14 20:25:03 -0300

    fix: env app port enviroment

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   })
 
   const configService = app.get(EnvService)
-  const port = configService.get('PORT')
+  const port = configService.get('APP_PORT')
 
   app.enableCors({
     origin: '*',
