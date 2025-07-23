924518ce5bfe114c16f7e3880d7a183c197987e9
João <joao.alcantara@fabricainfo.com>
2025-07-23 20:59:29 -0300

    fix: remove authz module

--- src/authz/company.ts
+++
@@ -1,4 +0,0 @@
-import { SetMetadata } from '@nestjs/common'
-
-export const COMPANY_KEY = 'companyId'
-export const Company = (companyId: string) => SetMetadata(COMPANY_KEY, companyId)
--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common'
 import { EventEmitterModule } from '@nestjs/event-emitter'
 import { DocumentTypeCreatedListener } from './document-type-created.listener'
-import { AuthzModule } from '@/authz/authz.module'
 
 @Module({
-  imports: [EventEmitterModule.forRoot(), AuthzModule],
+  imports: [EventEmitterModule.forRoot()],
   providers: [DocumentTypeCreatedListener],
   exports: [EventEmitterModule],
 })
